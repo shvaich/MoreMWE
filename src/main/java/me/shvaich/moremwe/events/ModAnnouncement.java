f5a78e2cc5aaa76122885afc16f1c7023a47dd77
shvaich <260186867+shvaich@users.noreply.github.com>
2026-07-13 16:23:14 +0300

    added a slight delay before sending the message

--- src/main/java/me/shvaich/moremwe/events/ModAnnouncement.java
+++ src/main/java/me/shvaich/moremwe/events/ModAnnouncement.java
@@ -1,5 +1,6 @@
 package me.shvaich.moremwe.events;
 
+import me.shvaich.moremwe.commands.MyAbstractCommand;
 import me.shvaich.moremwe.utils.ChatUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.EnumChatFormatting;
@@ -9,12 +10,17 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class ModAnnouncement {
 
+    private int ticksDelay = 35;
+
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent e) {
         final Minecraft mc = Minecraft.getMinecraft();
-        if (mc.theWorld != null && mc.thePlayer != null) {
-            MinecraftForge.EVENT_BUS.unregister(this);
-            ChatUtil.addChatMessage(ChatUtil.getModTag() + EnumChatFormatting.GRAY + "view /moremwe help");
+        if (mc.theWorld != null && mc.thePlayer != null && e.phase == TickEvent.Phase.START) {
+            if (ticksDelay <= 0) {
+                MinecraftForge.EVENT_BUS.unregister(this);
+                ChatUtil.addChatMessage(MyAbstractCommand.getCommandChatComponent(ChatUtil.getModTag() + EnumChatFormatting.GRAY + "view /moremwe help", "/moremwe help"));
+            }
+            ticksDelay--;
         }
     }
 }
